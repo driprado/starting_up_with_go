d4bfa31db2b302d07ddc9f12bffb9e99a888c1a4
Dri Prado <adriabp.br@gmail.com>
2020-11-08 21:58:56 +0000

    Multidimentional slices

--- 8_go_slices/go_slices.go
+++ 8_go_slices/go_slices.go
@@ -43,8 +43,35 @@ func main() {
 	// To remove an item from slice we have to "cut it out", as in using append and "jumping" the item you need to delete
 	sl = append(sl[0:10], sl[11:]...) // "jump item 10, therefore removing it"
 	fmt.Println(sl)
-	// If you have to do this multiple times, you can create a delete function:
+	// If you have to do this multiple times, you can create a delete function: "deleteFromSlice"
 	sl = deleteFromSlice(sl, 2)
+
+	//// Using make to create a slice
+	// https://golang.org/pkg/builtin/#make
+	// make alows you to set lenght and capacity of slice
+	// Slice: The size specifies the length. The capacity of the slice is
+	// equal to its length. A second integer argument may be provided to
+	// specify a different capacity
+	// make([]int, 0, 10) allocates an underlying array of size 10 and returns a slice of length 0 and capacity 10
+	x := make([]int, 10, 100)
+	fmt.Println(x)      // [0 0 0 0 0 0 0 0 0 0] make initializes the slice with value 0 if integer
+	fmt.Println(len(x)) // 10
+	fmt.Println(cap(x)) // 100
+
+	//// Multidimentional slices
+	// Multidimentional slices are slice of slices
+	sample := [2][3]int{{1001, 1002, 1003}, {1004, 1005, 1006}}
+
+	fmt.Printf("Number of rows in array: %d\n", len(sample))
+	fmt.Printf("Number of columns in array: %d\n", len(sample[0]))
+	fmt.Printf("Total number of elements in array: %d\n", len(sample)*len(sample[0]))
+
+	for _, row := range sample { // for each slice in sample
+		for _, item := range row { // for each item inside of each slice in sample
+			fmt.Println(item)
+		}
+	}
+
 }
 
 func deleteFromSlice(s []int, index int) []int {
