3baaa7153fbeb3ef9d55b79af93a8856b02b1450
Dri Prado <adriabp.br@gmail.com>
2020-11-11 07:10:48 +0000

    Methods and interfaces

--- go_interfaces/go_interfaces.go
+++ go_interfaces/go_interfaces.go
@@ -1,11 +1,8 @@
+// // Reading:
+
+// https://golang.org/ref/spec#Interface_types
+// https://www.alexedwards.net/blog/interfaces-explained
+
 // What are interfaces?
 // interface is a type in Go
 // It defines and describes the exact methods that some other type must have.
-
-
-
-
-
-
-// // References
-https://www.alexedwards.net/blog/interfaces-explained
--- go_methods/go_methods.go
+++ go_methods/go_methods.go
@@ -1,22 +1,40 @@
+//// Reading
+// https://www.youtube.com/watch?v=i3o4G4bmqPc 			@todo: create example
+// https://www.youtube.com/watch?v=93f9_bJQdHk			@todo: create example@todo-make example
+// https://golang.org/ref/spec#Method_declarations	@done
+// https://golang.org/ref/spec#Method_sets
+// https://golang.org/doc/faq#methods_on_values_or_pointers
+
+//// Notes:
+// ToSelf: Read code below then comeback to Notes
+// A method is a function with a receiver.
+// A method declaration associates the method with the receiver (ex: struct)
+
+// There are two types of methods in Go:
+// - Pointer receivers
+// 		.	capable of changing values in the receiver's base type (ex: structs)
+// - Value receivers
+// 		. perform operations with receiver values, but doesn't permanently change the receiver's base type
+
 package main
 
 import (
 	"fmt"
-	"math"
 )
 
+// Example 1: // @todo: change types and variables names, make it meaningful
 type myStruct struct {
-	X, Y float64
+	X, Y int
 }
 
-// A method is a function with a special receiver argument.
-// The receiver appears in its own argument list between the func keyword and the method name.
-
-func (s myStruct) myOperation() float64 { // Receiver: (s myStruct), // Method: myOperation(), //Returns: float64
-	return math.Sqrt(s.X*s.X + s.Y*s.Y)
+// (s myStruct) ==> receiver, a reference to the struct above
+// myOperation ==> method name
+// float64 ==> return type
+func (s myStruct) myOperation() int {
+	return ((s.X * s.X) * (s.Y * s.Y))
 }
 
 func main() {
-	s := myStruct{3, 4}
+	s := myStruct{2, 5}
 	fmt.Println(s.myOperation())
 }
