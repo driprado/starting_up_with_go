76fdab5756707625ced4c46b9e83a2dcdf0f3fa4
Dri Prado <adriabp.br@gmail.com>
2020-11-13 06:24:20 +0000

    Maps

--- 8_go_data_structures/go_maps/go_maps.go
+++ 8_go_data_structures/go_maps/go_maps.go
@@ -1,14 +1,24 @@
-// https://golang.org/ref/spec#Map_types
+//////////////////////////////////////// Readup:
+// https://golang.org/ref/spec#Map_types - @done
 // https://blog.golang.org/maps
-// Unordered group of elements of one type, called the element type, indexed by a set of unique keys of another type.
-// map[KeyType]ValueType Ex: map[string]int
+
+//////////////////////////////////////// Notes:
+// A map is an unordered group of elements.
+// Maps are indexed by a set unique keys of one type that are mapped to values of another type.
+// MapType = "map" "[" KeyType "]" ElementType .
+// Ex: map[string]int // maps strings to integers
+// A new, empty map value is made using the built-in function make, which takes the map type and an optional capacity hint as arguments:
+// Ex: make(map[string]int, 100)
+// The initial capacity does not bound its size: maps grow to accommodate the number of items stored in them, with the exception of nil maps.
+// A nil map is equivalent to an empty map except that no elements may be added.
 
 package main
 
 import "fmt"
 
 func main() {
-	// Example 1
+
+	//////////////////////////////////////// Example 1:
 	// phoneList maps names to phone numbers
 	phoneList := map[string]int{
 		"Anna":     404765123,
@@ -17,16 +27,20 @@ func main() {
 		"Danielle": 404656987,
 		"Ellie":    404434982,
 	}
-	fmt.Println(phoneList)          // Print all Keys and values
-	fmt.Println(phoneList["Ellie"]) // Print value for key "Ellie"
 
-	// If you query for a key that does not exist, in the case of values being inegers, map will return 0 instead of an error:
+	fmt.Println(phoneList)          // map[Anna:404765123 Beatrice:401234321 Carol:404453655 Danielle:404656987 Ellie:404434982]
+	fmt.Println(phoneList["Ellie"]) // 404434982
+
+	// If you query for a key that does not exist, if values are integers, map will return 0 instead of an error:
 	fmt.Println(phoneList["Fiona"]) // 0
-	// To check if the key/value you're querying for exist in the map, you can use the boolian `ok`:
-	value, ok := phoneList["Fiona"]
-	fmt.Println(value) // 0
-	fmt.Println(ok)    // false
 
+	// Verifying if a key/value pair exist in the map:
+	// To check if a key/value pair exist in the map, you can use the boolian `ok`:
+	value, ok := phoneList["Fiona"]
+	fmt.Println(value) // 0 // values is 0 as the key "Fiona" doesn't exist
+	fmt.Println(ok)    // false // value is not ok, it does not exist, nor the key
+
+	// Verifying an existing key:
 	value, ok = phoneList["Anna"]
 	fmt.Println(value) // 404765123
 	fmt.Println(ok)    // true
@@ -35,12 +49,12 @@ func main() {
 	if value, ok = phoneList["Fiona"]; ok {
 		fmt.Println("The phone number is:", value)
 	} else {
-		fmt.Println("Invalid key")
+		fmt.Println("Invalid key") // in this case this will be printed
 	}
 
-	// Example 2
-	// Use the builtin make function to initialise an empty map:
-	// The make function allocates and initializes a hash map data structure and returns a map value that points to it.
+	//////////////////////////////////////// Example 2:
+	// Use the builtin 'make' function to initialise an empty map:
+	// The make function allocates and initializes a hash map data structure and returns a map value that points to it
 	recordGoals := make(map[string]int)
 
 	recordGoals = map[string]int{
@@ -51,39 +65,59 @@ func main() {
 
 	// Individually adding key/vaues to a map:
 	recordGoals["Marta"] = 108
-	fmt.Println(recordGoals)
+	fmt.Println(recordGoals)        // map[Marta:108 Neymar:64 Pele:77 Ronaldo:62]
 	theBest := recordGoals["Marta"] // Assign the value of Marta to theBest variable (outside map)
-	fmt.Println(recordGoals)
-	fmt.Println(theBest) //108
+	fmt.Println(recordGoals)        // map[Marta:108 Neymar:64 Pele:77 Ronaldo:62] // not alrered
+	fmt.Println(theBest)            //108
 
-	// len function returns on the number of items in a map:
+	// len function returns the number of items in a map:
 	fmt.Println(len(recordGoals)) // 4
 
 	// delete function removes an entry from the map:
 	delete(recordGoals, "Neymar")
 	fmt.Println(recordGoals) // map[Marta:108 Pele:77 Ronaldo:62]
 
-	//// Example 3 - Exploiting zero values
-	// A gym has as promotion to give 2 weeks free for new members only.
-	// We need to make check if an email has already been signed
+	//////////////////////////////////////// Example 3:
+	// You're a headhunter and have a map of programmers and their preferred programming languages
+	// You want to know which are the programmers that know Golang:
 
-	type Member struct {
-		Next  *Member
-		Email string
+	// RE-think this
+
+	type Coders struct {
+		Name      string
+		PrefLangs []string
 	}
 
-	var member *Member
-
-	registered := make(map[*Member]bool)
-	for person := member; person != nil; person = person.Next {
-		if registered[person] {
-			fmt.Println("already registered")
-			break
-		}
-		registered[person] = true
-		fmt.Println(person.Email)
+	coder0 := Coders{
+		Name:      "Ada",
+		PrefLangs: []string{"math", "differential", "calculus", "discrete"},
 	}
 
-	// Preencher Member com varias pessoas aqui!!!!
-	// Depois continuar daqui: https://blog.golang.org/maps
+	coder1 := Coders{
+		Name:      "Hoper",
+		PrefLangs: []string{"assembly", "cobol", "contran"},
+	}
+
+	coder2 := Coders{
+		Name:      "Borg",
+		PrefLangs: []string{"assembly", "cobol", "C", "bash"},
+	}
+
+	coder3 := Coders{
+		Name:      "Hamilton",
+		PrefLangs: []string{"assembly", "corona"},
+	}
+
+	coder4 := Coders{
+		Name:      "Korbes",
+		PrefLangs: []string{"ruby", "python", "go"},
+	}
+
+	coder5 := Coders{
+		Name:      "Prado",
+		PrefLangs: []string{"ruby", "python", "go"},
+	}
+
+	fmt.Println(coder0)
+
 }
