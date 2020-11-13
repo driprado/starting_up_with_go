bcec812474bb09d6349872474f3555f0364da7a9
Dri Prado <adriabp.br@gmail.com>
2020-11-13 02:45:12 +0000

    Touch up

--- 9_go_methods/pointer_receivers/main.go
+++ 9_go_methods/pointer_receivers/main.go
@@ -6,18 +6,23 @@ import "fmt"
 // https://www.youtube.com/watch?v=i3o4G4bmqPc 			@done
 // https://www.youtube.com/watch?v=93f9_bJQdHk			@done
 // https://golang.org/ref/spec#Method_declarations	@done
-// https://golang.org/ref/spec#Method_sets
+// https://golang.org/ref/spec#Method_sets					@done
 // https://golang.org/doc/faq#methods_on_values_or_pointers
 
 //// Notes:
 // A method is a function with a receiver.
 // A method declaration associates the method with the receiver (ex: struct)
+// When defining a method on a type, the receiver behaves exactly as if it were an argument to the method.
 
 // There are two types of methods in Go:
 // - Pointer receivers
 // 		.	capable of changing values in the receiver's base type (ex: structs)
+// func (s *MyStruct) pointerMethod() { } // method on pointer
+
 // - Value receivers
 // 		. perform operations with receiver values, but doesn't permanently change the receiver's base type
+// func (s MyStruct)  valueMethod()   { } // method on value
+
 // Example 1: Pointer receiver (modify values of the base type, in this case, the 'trip' struct)
 type trip struct {
 	distance        float64
