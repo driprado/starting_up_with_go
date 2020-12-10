1cd2c0f737a6d6ebfb478b03f6206753f2e3d117
Dri Prado <adriabp.br@gmail.com>
2020-12-10 11:19:37 +0000

    Quick push

--- 8_go_data_structures/go_structs/main.go
+++ 8_go_data_structures/go_structs/main.go
@@ -1,4 +1,4 @@
-@here
+// @here
 
-https://www.udemy.com/course/learn-how-to-code/learn/lecture/11922206#overview
-https://golang.org/ref/spec#Struct_types
+// https://www.udemy.com/course/learn-how-to-code/learn/lecture/11922206#overview
+// https://golang.org/ref/spec#Struct_types
